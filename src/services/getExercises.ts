3d26470e2e8197d0f0106b571dd34e665cb10518
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-05 20:42:15 -0300

    feat: add getExercise service

---
+++ src/services/getExercises.ts
@@ -0,0 +1,25 @@
+const getExercises = async () => {
+  const url = "https://exercisedb.p.rapidapi.com/exercises?limit=10";
+  const rapidApiKey = process.env.RAPID_API_KEY;
+  if (!rapidApiKey) {
+    throw new Error("No API key specified");
+  }
+  const options: RequestInit = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": rapidApiKey,
+      "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+    },
+    next: { revalidate: 43200 },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default getExercises;
