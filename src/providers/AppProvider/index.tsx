53fe5071e39f3c92d538924a2c27d2ef1a306fef
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-05 19:01:27 -0300

    feat: add Session Provider to App Provider

--- src/providers/AppProvider/index.tsx
+++ src/providers/AppProvider/index.tsx
@@ -1,11 +1,17 @@
 "use client";
 import { NextUIProvider } from "@nextui-org/react";
+import { SessionProvider } from "next-auth/react";
+
 interface AppProviderProps {
   children: React.ReactNode;
 }
 
 const AppProvider = ({ children }: AppProviderProps) => {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <SessionProvider>
+      <NextUIProvider>{children}</NextUIProvider>;
+    </SessionProvider>
+  );
 };
 
 export default AppProvider;
