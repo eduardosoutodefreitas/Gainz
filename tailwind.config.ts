53bfe90f3c750071775c404dbc441a40350488b1
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-05 19:03:32 -0300

    chore: add dark secondary bg

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,11 @@ const config: Config = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkSecondaryBg: "#080808",
+      },
+    },
   },
   darkMode: "class",
   plugins: [
