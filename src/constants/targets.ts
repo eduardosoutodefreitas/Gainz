9b43f39cfd2c152c1db8fc69312f1dfe23e70e00
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-06 11:49:01 -0300

    feat: add targets list

---
+++ src/constants/targets.ts
@@ -0,0 +1,25 @@
+import { Target } from "@/types/TargetTypes";
+
+const targets: Target[] = [
+  { label: "Abductors", value: "abductors" },
+  { label: "Abs", value: "abs" },
+  { label: "Adductors", value: "adductors" },
+  { label: "Biceps", value: "biceps" },
+  { label: "Calves", value: "calves" },
+  { label: "Cardiovascular System", value: "cardiovascular system" },
+  { label: "Delts", value: "delts" },
+  { label: "Forearms", value: "forearms" },
+  { label: "Glutes", value: "glutes" },
+  { label: "Hamstrings", value: "hamstrings" },
+  { label: "Lats", value: "lats" },
+  { label: "Levator Scapulae", value: "levator scapulae" },
+  { label: "Pectorals", value: "pectorals" },
+  { label: "Quads", value: "quads" },
+  { label: "Serratus Anterior", value: "serratus anterior" },
+  { label: "Spine", value: "spine" },
+  { label: "Traps", value: "traps" },
+  { label: "Triceps", value: "triceps" },
+  { label: "Upper Back", value: "upper back" },
+];
+
+export default targets;
