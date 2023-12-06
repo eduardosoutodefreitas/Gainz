0957606499582bc06882e24a32b9538a4354e514
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-06 11:51:05 -0300

    feat: add filterExercisesByTarget function and Select Component

--- src/app/components/AddExercise/AddExercisesContainer.tsx
+++ src/app/components/AddExercise/AddExercisesContainer.tsx
@@ -1,16 +1,45 @@
+"use client";
 import { Exercise } from "@/types/ExercisesTypes";
 import ExerciseCard from "./ExerciseCard";
+import targets from "@/constants/targets";
+import { Select, SelectItem } from "@nextui-org/react";
+import { useState } from "react";
 
 interface AddExercisesContainerProps {
   exercises: Exercise[];
 }
 
 const AddExercisesContainer = ({ exercises }: AddExercisesContainerProps) => {
+  const [selectedTarget, setSelectedTarget] = useState("");
+
+  const filterExercisesByTarget = (target: string) => {
+    if (target.length > 0) {
+      return exercises.filter((exercise) => exercise.target === target);
+    }
+    return exercises;
+  };
+  const filteredExercises = filterExercisesByTarget(selectedTarget);
+  const handleSelectionChange = (e: any) => {
+    setSelectedTarget(e.target.value);
+  };
+
   return (
-    <div className='bg-darkSecondaryBg w-full max-w-md p-5 max-h-[75vh] overflow-y-auto rounded-sm'>
-      {exercises.map((exercise) => (
-        <ExerciseCard exercise={exercise} key={exercise.id} />
-      ))}
+    <div className='bg-darkSecondaryBg w-full max-w-md min-w-[324px] p-5 h-[600px] overflow-y-auto scrollbar-thin scrollbar-thumb-secondary scrollbar-thumb-rounded-sm scrollbar-track-black rounded-sm'>
+      <Select
+        aria-labelledby='Select a muscle target'
+        items={targets}
+        variant='underlined'
+        placeholder='Filter exercises by muscle target'
+        className='w-full mb-8 bg'
+        onChange={handleSelectionChange}
+      >
+        {(target) => <SelectItem key={target.value}>{target.label}</SelectItem>}
+      </Select>
+      <div>
+        {filteredExercises.map((exercise) => (
+          <ExerciseCard exercise={exercise} key={exercise.id} />
+        ))}
+      </div>
     </div>
   );
 };
