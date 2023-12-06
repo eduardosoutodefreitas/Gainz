f97c8647013373dc826daa6e92d848dd914213a5
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-06 10:20:35 -0300

    style: add max height and overflow auto to Add Exercises Container

--- src/app/components/AddExercise/AddExercisesContainer.tsx
+++ src/app/components/AddExercise/AddExercisesContainer.tsx
@@ -7,7 +7,7 @@ interface AddExercisesContainerProps {
 
 const AddExercisesContainer = ({ exercises }: AddExercisesContainerProps) => {
   return (
-    <div className='bg-darkSecondaryBg w-full max-w-md p-5'>
+    <div className='bg-darkSecondaryBg w-full max-w-md p-5 max-h-[75vh] overflow-y-auto rounded-sm'>
       {exercises.map((exercise) => (
         <ExerciseCard exercise={exercise} key={exercise.id} />
       ))}
