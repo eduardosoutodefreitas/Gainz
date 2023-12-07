4ec1a820bb6c270f761db39f979b455fbfcc726b
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-07 16:51:15 -0300

    refactor: change getUserEmail function

--- src/services/getUserEmail.ts
+++ src/services/getUserEmail.ts
@@ -3,8 +3,6 @@ import { getServerSession } from "next-auth";
 
 const getUserEmail = async () => {
   const session = await getServerSession(authOptions);
-  const isLoggedIn = !!session?.user;
-  console.log(isLoggedIn);
   const userEmail = session?.user?.email;
   return {
     userEmail,
