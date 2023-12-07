61eda04bdb9b8fc987d79f1c8fc830b2f1873f06
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-07 19:45:33 -0300

    feat: add login verification, toast feedback and loading state

--- src/hooks/useWorkoutPreview.ts
+++ src/hooks/useWorkoutPreview.ts
@@ -4,34 +4,56 @@ import { useWorkout } from "@/context/WorkoutContext";
 import { WorkoutData } from "@/types/WorkoutDataTypes";
 import { useSession } from "next-auth/react";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 const useWorkoutPreview = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [workoutName, setWorkoutName] = useState("");
   const { data } = useSession();
-  const { addedExercises } = useWorkout();
+  const { addedExercises, clearAddedExercises } = useWorkout();
   const buttonIsDisabled = !(
     addedExercises.length > 0 && workoutName.trim().length > 0
   );
+  const isLoggedIn = !!data?.user;
+  const showLoginToast = () => {
+    toast.error("Please login to save your workouts");
+  };
   const handleSaveWorkoutClick = async () => {
-    const newWorkoutExercisesList = addedExercises.map(
-      (exercise) => exercise.id
-    );
-    const workout: WorkoutData = {
-      exercisesList: newWorkoutExercisesList,
-      name: workoutName,
-    };
-    const userEmail = data?.user?.email;
-    if (!userEmail) {
-      console.error("Usuário não encontrado.");
-      return;
+    if (isLoggedIn) {
+      try {
+        setIsLoading(true);
+        const newWorkoutExercisesList = addedExercises.map(
+          (exercise) => exercise.id
+        );
+        const workout: WorkoutData = {
+          exercisesList: newWorkoutExercisesList,
+          name: workoutName,
+        };
+        const userEmail = data?.user?.email;
+        if (!userEmail) {
+          console.error("Usuário não encontrado.");
+          return;
+        }
+        await createWorkoutAction(userEmail, workout);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+        setWorkoutName("");
+        clearAddedExercises();
+        toast.success("Workout created successfully!");
+      }
+    } else {
+      showLoginToast();
     }
-    await createWorkoutAction(userEmail, workout);
   };
 
   return {
     workoutName,
     buttonIsDisabled,
     addedExercises,
+    isLoading,
+    isLoggedIn,
     setWorkoutName,
     handleSaveWorkoutClick,
   };
