5ac70eff9cc42d902daae6b653d490e04d14b130
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-07 20:01:16 -0300

    feat: add userWorkouts length verification

--- src/app/myWorkouts/UserWorkoutsList.tsx
+++ src/app/myWorkouts/UserWorkoutsList.tsx
@@ -23,6 +23,15 @@ const UserWorkoutsList = ({ workouts, userEmail }: UserWorkoutsListProps) => {
 
   return (
     <div className='w-full p-5 rounded-sm bg-darkSecondaryBg max-h-[600px] overflow-y-auto scrollbar-thin scrollbar-thumb-secondary scrollbar-thumb-rounded-sm scrollbar-track-black'>
+      {userWorkouts.length === 0 && (
+        <p className='text-center text-lg'>
+          No saved workouts. Create new custom workouts by clicking on the{" "}
+          <span className='text-secondary font-medium'>
+            {"'Create new workout'"}
+          </span>{" "}
+          button.
+        </p>
+      )}
       {userWorkouts.map((workout) => (
         <WorkoutCard
           onDelete={() => handleDeleteButtonClick(workout.id)}
