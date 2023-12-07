9e088d204687648d8e6349c078d89d210e03d59c
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-07 16:43:07 -0300

    feat: add UserWorkoutList Component

---
+++ src/app/myWorkouts/UserWorkoutsList.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Workout } from "@prisma/client";
+import WorkoutCard from "../components/WorkoutCard/WorkoutCard";
+import { useState } from "react";
+import { deleteWorkoutAction } from "../actions/DeleteUserWorkout/_actions";
+
+interface UserWorkoutsListProps {
+  workouts: Workout[];
+  userEmail: string;
+}
+
+const UserWorkoutsList = ({ workouts, userEmail }: UserWorkoutsListProps) => {
+  const [userWorkouts, setUserWorkouts] = useState<Workout[]>(workouts);
+
+  const handleDeleteButtonClick = async (workoutId: string) => {
+    await deleteWorkoutAction(userEmail, workoutId);
+    const updatedWorkouts = userWorkouts.filter(
+      (workout) => workout.id !== workoutId
+    );
+    setUserWorkouts(updatedWorkouts);
+  };
+
+  return (
+    <div className='w-full p-5 rounded-sm bg-darkSecondaryBg max-h-[600px] overflow-y-auto scrollbar-thin scrollbar-thumb-secondary scrollbar-thumb-rounded-sm scrollbar-track-black'>
+      {userWorkouts.map((workout) => (
+        <WorkoutCard
+          onDelete={() => handleDeleteButtonClick(workout.id)}
+          key={workout.id}
+          workout={workout}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default UserWorkoutsList;
