58bf5fd6ed6b2be0832317b9af8fa92a6e92bc93
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-08 11:34:18 -0300

    feat: add Hero section to Home page

---
+++ src/app/components/Hero/Hero.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroCta from "./HeroCta";
+import HeroPictures from "./HeroPictures";
+
+const Hero = () => {
+  return (
+    <section className='lg:h-[calc(100vh-80px)] w-full flex flex-col gap-24 lg:gap-0 lg:flex-row items-center justify-between xl:justify-around py-20 lg:py-0 px-5 mb-10'>
+      <HeroCta />
+      <HeroPictures />
+    </section>
+  );
+};
+
+export default Hero;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Footer from "./components/Footer/Footer";
+import Hero from "./components/Hero/Hero";
 
 const Home = () => {
   return (
-    <main>
+    <main className='w-full container mx-auto'>
+      <Hero />
       <Footer />
     </main>
   );
