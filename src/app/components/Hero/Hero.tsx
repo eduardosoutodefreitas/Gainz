35f5e2604d3678b88f64ae6c0dde810aef788260
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-08 13:53:52 -0300

    style: change hero section padding

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -4,7 +4,7 @@ import HeroPictures from "./HeroPictures";
 
 const Hero = () => {
   return (
-    <section className='lg:h-[calc(100vh-80px)] w-full container mx-auto flex flex-col gap-24 lg:gap-0 lg:flex-row items-center justify-between xl:justify-around py-20 lg:py-0 px-5 mb-10'>
+    <section className='lg:h-[calc(100vh-80px)] w-full container mx-auto flex flex-col gap-24 lg:gap-0 lg:flex-row items-center justify-between xl:justify-around py-20 lg:py-0 px-8 mb-10'>
       <HeroCta />
       <HeroPictures />
     </section>
