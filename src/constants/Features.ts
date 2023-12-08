c5ae17deaac6b3ee434d6d0284c822dc4e0dcebd
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-08 12:55:22 -0300

    feat: add feature constant

---
+++ src/constants/Features.ts
@@ -0,0 +1,16 @@
+import { Feature } from "@/types/FeatureTypes";
+
+const features: Feature[] = [
+  {
+    title: "Customize Easily",
+    description:
+      "Choose from 1000+ exercises and effortlessly add them to your personalized workout plan.",
+  },
+  {
+    title: "Transform Journey",
+    description:
+      "Experience the power of Gainz as it transforms your fitness journey, your way.",
+  },
+];
+
+export default features;
