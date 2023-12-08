c401dfbab0c1d91c25a0f320cbe7fd4f67a70f28
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-08 14:08:42 -0300

    style: add flex col to HeroCountUp component

--- src/app/components/Hero/HeroCountUp.tsx
+++ src/app/components/Hero/HeroCountUp.tsx
@@ -5,7 +5,7 @@ import CountUp from "react-countup";
 
 const HeroCountUp = () => {
   return (
-    <div className='flex md:max-w-sm justify-between gap-5 items-center'>
+    <div className='flex md:max-w-sm flex-col xs:flex-row justify-between gap-5 items-center'>
       <div className='text-center'>
         <h3 className='text-2xl font-semibold'>Exercises</h3>
         <span className='font-medium text-xl'>
