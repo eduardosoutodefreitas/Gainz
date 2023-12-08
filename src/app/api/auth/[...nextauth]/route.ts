6838046c944c4887a7ca5123a55d673c8960e17c
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-08 13:16:51 -0300

    fix: add specific file to authOptions

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,38 +1,5 @@
-import NextAuth, { AuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { Adapter } from "next-auth/adapters";
-import prisma from "@/lib/prisma";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-
-export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prisma) as Adapter,
-  providers: [
-    (() => {
-      if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
-        throw new Error(
-          "As variáveis de ambiente do Google não estão configuradas corretamente."
-        );
-      }
-
-      return GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      });
-    })(),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-  callbacks: {
-    async session({ session, user }) {
-      session.user = { ...session.user, id: user.id } as {
-        id: string;
-        name: string;
-        email: string;
-      };
-
-      return session;
-    },
-  },
-};
+import NextAuth from "next-auth";
+import { authOptions } from "@/auth/authOptions";
 
 const handler = NextAuth(authOptions);
 
