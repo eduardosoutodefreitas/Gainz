bdbaf39506d3a1c8989a50575bb2e739d5c66786
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-08 12:54:01 -0300

    feat: add Balancer to Hero Cta

--- src/app/components/Hero/HeroCta.tsx
+++ src/app/components/Hero/HeroCta.tsx
@@ -1,26 +1,32 @@
 import { Button } from "@nextui-org/react";
 import React from "react";
 import HeroCountUp from "./HeroCountUp";
+import Balancer from "react-wrap-balancer";
+import Link from "next/link";
 
 const HeroCta = () => {
   return (
     <div className='w-full lg:w-1/2 min-w-[250px] flex flex-col items-center lg:items-start gap-5 text-center lg:text-start'>
       <h1 className='xl:text-7xl md:text-5xl text-4xl font-black'>
-        Achieve Your Fitness Goals with Gainz
+        <Balancer>Achieve Your Fitness Goals with Gainz</Balancer>
       </h1>
       <p className='sm:text-lg'>
-        Get personalized workouts with 1000+ exercises to choose from. Transform
-        your fitness journey with ease and simplicity.
+        <Balancer>
+          Get personalized workouts with 1000+ exercises to choose from.
+          Transform your fitness journey with ease and simplicity.
+        </Balancer>
       </p>
-      <Button
-        className='my-3'
-        size='lg'
-        radius='sm'
-        variant='shadow'
-        color='secondary'
-      >
-        Start creating customized workouts
-      </Button>
+      <Link href='/create-workout'>
+        <Button
+          className='my-3'
+          size='lg'
+          radius='sm'
+          variant='shadow'
+          color='secondary'
+        >
+          Start creating customized workouts
+        </Button>
+      </Link>
       <HeroCountUp />
     </div>
   );
