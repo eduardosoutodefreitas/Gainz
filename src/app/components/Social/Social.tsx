15f1c3fc2360c75dd84a6aa1c5b3bb9bb881df1c
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-11 14:45:05 -0300

    refactor: add extrastyles props to Social component and remove border

--- src/app/components/Social/Social.tsx
+++ src/app/components/Social/Social.tsx
@@ -3,11 +3,12 @@ import Link from "next/link";
 interface SocialProps {
   icon: React.ReactNode;
   link: string;
+  extraStyles?: string;
 }
-const Social = ({ icon, link }: SocialProps) => {
+const Social = ({ icon, link, extraStyles = "" }: SocialProps) => {
   return (
     <Link
-      className='p-2 rounded-md border border-white hover:border-secondary hover:text-secondary hover:-translate-y-1 transition-all duration-150'
+      className={`hover:text-secondary hover:-translate-y-1 transition-all duration-150 ${extraStyles}`}
       href={link}
     >
       {icon}
