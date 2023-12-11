6d2728a49a278699cc0502a9884a128bea24ae61
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-11 14:54:05 -0300

    style: change footer mobile style

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -7,9 +7,9 @@ import Social from "../Social/Social";
 const Footer = () => {
   return (
     <footer className='p-5'>
-      <div className='w-full container mx-auto flex flex-col items-center gap-5 sm:flex-row justify-center sm:justify-between'>
+      <div className='w-full container mx-auto flex flex-col items-center gap-8 sm:flex-row justify-center sm:justify-between'>
         <Logo size='small' />
-        <div className='w-full sm:w-4/6 max-w-xl flex justify-between items-center gap-5 xs:gap-0'>
+        <div className='w-full sm:w-4/6 max-w-xl flex flex-col xs:flex-row justify-between items-center gap-5 xs:gap-0'>
           <Link
             className='pb-1 border-b font-medium hover:text-secondary hover:border-secondary transition-all duration-150'
             href='/'
@@ -17,19 +17,19 @@ const Footer = () => {
             Home
           </Link>
           <Link
-            className='pb-1 border-b font-medium hover:text-secondary text-center hover:border-secondary transition-all duration-150'
-            href='/create-workout'
-          >
-            Create workout
-          </Link>
-          <Link
             className='pb-1 border-b font-medium hover:text-secondary hover:border-secondary transition-all duration-150'
             href='/about'
           >
             About
           </Link>
+          <Link
+            className='pb-1 border-b font-medium hover:text-secondary text-center hover:border-secondary transition-all duration-150'
+            href='/create-workout'
+          >
+            Create workout
+          </Link>
         </div>
-        <div className='w-full xs:w-[300px] flex justify-between items-center'>
+        <div className='w-full xs:w-[200px] flex items-center justify-center gap-5'>
           {socials.map((social) => (
             <Social key={social.name} icon={social.icon} link={social.link} />
           ))}
