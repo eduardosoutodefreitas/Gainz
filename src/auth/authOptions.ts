8099842a43ba41bb53b6b5c55ea85c8d27522c6e
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-11 13:26:17 -0300

    fix: fix enviroment variables error

--- src/auth/authOptions.ts
+++ src/auth/authOptions.ts
@@ -7,18 +7,10 @@ import prisma from "@/lib/prisma";
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma) as Adapter,
   providers: [
-    (() => {
-      if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
-        throw new Error(
-          "As variáveis de ambiente do Google não estão configuradas corretamente."
-        );
-      }
-
-      return GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      });
-    })(),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
