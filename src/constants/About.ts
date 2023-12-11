967eacb5052e695ed559483b5ba7ee005f65c409
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-11 14:45:38 -0300

    feat: add aboutinfos constant

---
+++ src/constants/About.ts
@@ -0,0 +1,26 @@
+import About from "@/types/AboutTypes";
+
+const aboutInfos: About[] = [
+  {
+    title: "What is Gainz?",
+    content:
+      "Gainz is a platform for creating personalized workout plans, envisioned by me to make the process of creating a gym workout routine easier and more motivating. I designed it to provide users with a seamless experience, fostering motivation for their training sessions. The ultimate goal is to develop Gainz into a comprehensive platform for tracking progress in the gym. The idea originated from recognizing the importance of progressive overloading in the gym.",
+  },
+  {
+    title: "How it Works?",
+    content:
+      'Using Gainz is simple! Just click the "Login" button and sign in with your Google account. Once logged in, you can start creating your personalized workout plans. The platform is designed to be user-friendly, allowing you to easily navigate through the process of crafting your own fitness routines.',
+  },
+  {
+    title: "What devices are compatible with Gainz?",
+    content:
+      "Gainz is a web-based platform designed to work smoothly on all devices—desktops, laptops, tablets, and phones. No matter how small the screen, our platform adjusts to provide an optimal user experience. Whether you're planning your workouts on a computer or checking progress from your phone at the gym, Gainz has you covered with responsive design.",
+  },
+  {
+    title: "Can I share my workout plans with others?",
+    content:
+      "In the future, Gainz will become a social platform. Users can share and copy workout plans, creating a community where you find inspiration, follow others, and make fitness more collaborative. Exciting updates are on the way as Gainz transforms into a social fitness hub!",
+  },
+];
+
+export default aboutInfos;
