bbab5e01467f14b877db8d825fae4eeb735a3ac5
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-20 09:51:39 -0300

    feat: add sizes to ExerciseCard Image

--- src/app/components/ExerciseCard.tsx/ExerciseCard.tsx
+++ src/app/components/ExerciseCard.tsx/ExerciseCard.tsx
@@ -21,10 +21,12 @@ const ExerciseCard: React.FC<ExerciseCardProps> = ({
   imageSize = "medium",
 }: ExerciseCardProps) => {
   const imageSizeClass = {
-    small: "w-[90px] h-[90px] w-[100px] h-[100px]",
+    small: "w-[100px] h-[100px] ",
     medium: "w-[120px] h-[120px]",
     large: "w-[150px] h-[150px]",
   }[imageSize];
+  const sizes = "(max-width: 640px) 100vw, (max-width: 768px) 90vw, 80vw";
+
   return (
     <div className='relative flex items-center gap-5 mb-5 w-full p-3 hover:bg-default-50 rounded-md'>
       {showRemoveButton && (
@@ -36,13 +38,16 @@ const ExerciseCard: React.FC<ExerciseCardProps> = ({
         <Image
           fill
           style={{ objectFit: "cover" }}
+          sizes={sizes}
           alt={`${exercise.name} icon`}
           className='rounded-md'
           src={exercise.gifUrl}
         />
       </div>
       <div className='self-start w-[50%]'>
-        <p className='text-lg capitalize font-medium'>{exercise.name}</p>
+        <p className='text-sm xs:text-base sm:text-lg capitalize font-medium'>
+          {exercise.name}
+        </p>
         {showMuscleTarget && (
           <p className='text-sm capitalize font-medium text-secondary'>
             {exercise.target}
