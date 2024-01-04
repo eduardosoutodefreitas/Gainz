a854a544ffb9a8e6e2355440b7ef3154f83885f6
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2024-01-04 15:51:24 -0300

    refactor: refactor useWorkoutPreview custom Hook

--- src/hooks/useWorkoutPreview.ts
+++ src/hooks/useWorkoutPreview.ts
@@ -6,54 +6,58 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import toast from "react-hot-toast";
-
 const useWorkoutPreview = () => {
   const { push } = useRouter();
   const [isLoading, setIsLoading] = useState(false);
   const [workoutName, setWorkoutName] = useState("");
   const { data } = useSession();
   const { addedExercises, clearAddedExercises } = useWorkout();
-  const buttonIsDisabled = !(
-    addedExercises.length > 0 && workoutName.trim().length > 0
-  );
   const isLoggedIn = !!data?.user;
+
   const showLoginToast = () => {
     toast.error("Please login to save your workouts");
   };
+
   const handleSaveWorkoutClick = async () => {
-    if (isLoggedIn) {
-      try {
-        setIsLoading(true);
-        const newWorkoutExercisesList = addedExercises.map(
-          (exercise) => exercise.id
-        );
-        const workout: WorkoutData = {
-          exercisesList: newWorkoutExercisesList,
-          name: workoutName,
-        };
-        const userEmail = data?.user?.email;
-        if (!userEmail) {
-          console.error("Usuário não encontrado.");
-          return;
-        }
-        await createWorkoutAction(userEmail, workout);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setIsLoading(false);
-        setWorkoutName("");
-        clearAddedExercises();
-        toast.success("Workout created successfully!");
-        push("/myWorkouts");
-      }
-    } else {
+    if (!isLoggedIn) {
       showLoginToast();
+      return;
+    }
+
+    try {
+      setIsLoading(true);
+      const newWorkoutExercisesList = addedExercises.map(
+        (exercise) => exercise.id
+      );
+      const workout: WorkoutData = {
+        exercisesList: newWorkoutExercisesList,
+        name: workoutName,
+      };
+
+      const userEmail = data?.user?.email;
+      if (!userEmail) {
+        console.error("Usuário não encontrado.");
+        return;
+      }
+
+      await createWorkoutAction(userEmail, workout);
+      toast.success("Workout created successfully!");
+      push("/myWorkouts");
+    } catch (error) {
+      console.error("Error saving workout:", error);
+      toast.error("Error saving workout. Please try again later.");
+    } finally {
+      setIsLoading(false);
+      setWorkoutName("");
+      clearAddedExercises();
     }
   };
 
   return {
     workoutName,
-    buttonIsDisabled,
+    buttonIsDisabled: !(
+      addedExercises.length > 0 && workoutName.trim().length > 0
+    ),
     addedExercises,
     isLoading,
     isLoggedIn,
