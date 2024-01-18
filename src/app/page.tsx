29ee905d6ab8f69ddf2e10fc580f201baf2b5a52
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2024-01-18 19:16:01 -0300

    refactor: move Cta to components/Sections folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Footer from "./components/Footer/Footer";
 import Hero from "./components/Sections/Hero/Hero";
 import Features from "./components/Features/Features";
-import Cta from "./components/CtaSection/Cta";
+import Cta from "./components/Sections/CtaSection/Cta";
 
 const Home = () => {
   return (
